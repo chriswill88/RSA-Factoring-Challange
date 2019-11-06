2e544a4d68e110a3ec56e3fed04d1369ca62bf51
chriswill88 <williechri79@gmail.com>
2019-11-06 08:10:11 -0500

    [Update] added functionality for 0, and 1 and negative numbers and took away errors for prime numbers

--- factors.py
+++ factors.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
 from sys import argv
-import time
 """ this factors modual gets the first two factors of any number """
 
 
-def factor(line=""):
+def factor(num):
     """
         This factor function
         gets the factors of
         a number and
         prints them out
     """
-    num = int(line)
-    if num % 2 == 0:
+    # num = int(line)
+    if num == 0:
+        i = 0
+        print("{}={}*{}".format(num, i, num))
+        return
+    elif num == 1 or num == -1:
+        i = 1
+        print("{}={}*{}".format(num, i, int(num/i)))
+
+    elif num % 2 == 0:
         i = 2
+        print("{}={}*{}".format(num, i, int(num/i)))
+
     else:
-        for i in range(3, num, 2):
+        for i in range(3, int(num/2), 2):
             if num % i == 0:
-                break
-    print("{}={}*{}".format(num, i, int(num/i)))
+                print("{}={}*{}".format(num, i, int(num/i)))
+                return
 
 
 with open(argv[1]) as f:
     for line in f:
-        factor(line)
+        factor(int(line))
