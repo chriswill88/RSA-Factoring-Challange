7d0c715d73fb2c01f17a2ed8ac8900d4b7f6ab6f
chriswill88 <williechri79@gmail.com>
2019-11-06 01:05:34 -0500

    [Add] Readme, factors ex, rsa ex, and test directory

--- rsa.py
+++ rsa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
+import math
 """ this modual gets the first prime factors of any number """
 
 
@@ -15,13 +16,28 @@ def rsa(line):
         print("{}={}*{}".format(num, int(num/2), 2))
         return
 
-    for i in range(3, int(num/2), 2):
-        # print(i)
+    sq = int(math.sqrt(num))
+    print(sq)
+
+    if sq % 2 != 0:
+        sq += 1
+
+    HalfSq = int(sq/2)  # cuts sq in half
+
+    for i in range(sq, int(num/2), 2):
+        SqRootIt = i - sq  # iterator from sqaure root
+        # print("n = ", n)
         if num % i == 0:
-            for x in range(3, int(num/2), 2):
-                if (i % x == 0):
+            for x in range(3, i, 2):
+                if i % x == 0:
                     print("{}={}*{}".format(num, int(num/i), i))
                     return
+        if num % SqRootIt == 0:  # currently implimenting square root method
+            for  
+
+
+
+        
 
 
 with open(argv[1]) as f:
